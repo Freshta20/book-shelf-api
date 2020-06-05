72ee0971384977a883e01008f1270a9dcbb640b4
freshta <freshta.bashari@gmail.com>
2020-06-05 11:00:21 -0700

    added all the routs for catagories routes

--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,9 @@ const ctrl = require('../controllers')
 
 // Album controller routes
 router.get('/', ctrl.catagories.index)
-
+router.post('/', ctrl.catagories.create)
+router.get('/:id', ctrl.catagories.show)
+router.put('/:id', ctrl.catagories.update)
+router.delete('/:id', ctrl.catagories.destroy)
 
 module.exports = router
