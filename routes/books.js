9fa511f89ba6e5cd37778c9a5c863b1a63132adf
freshta <freshta.bashari@gmail.com>
2020-06-05 19:50:37 -0700

    added the show rout successfully

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const ctrl = require('../controllers');
 // Book controller routs
 router.get('/', ctrl.books.index)
 router.post('/new', ctrl.books.create)
-// router.get('/:id', ctrl.books.show)
+router.get('/:id', ctrl.books.show)
 // router.put('/:id', ctrl.books.update)
 // router.delete('/:id', ctrl.books.destroy)
 
