2e7eef263634aee8951b3343b917ded05f1f109c
freshta <freshta.bashari@gmail.com>
2020-06-05 20:14:13 -0700

    finished the book routes

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,7 @@ router.get('/', ctrl.books.index)
 router.post('/new', ctrl.books.create)
 router.get('/:id', ctrl.books.show)
 router.put('/:id', ctrl.books.update)
-// router.delete('/:id', ctrl.books.destroy)
+router.delete('/:id', ctrl.books.destroy)
 
 
 module.exports = router
