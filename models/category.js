61d5a64b93e646d9bf8ee246727efd5fdfdd0124
freshta <freshta.bashari@gmail.com>
2020-06-15 11:01:11 -0700

    added changes for heroku

---
+++ models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+  genre: {
+    type: String,
+    unique: true,
+    required: true, 
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  books: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+  }],
+})
+
+const Category = mongoose.model('Category', CategorySchema)
+module.exports = Category
