d23c69c460d9326ec169d1fe2d1daa93545ee4ad
freshta <freshta.bashari@gmail.com>
2020-06-15 10:26:39 -0700

    added some changes

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ mongoose.connect(connectionString, configOptions)
     .catch(err => console.log(`MongoDB connection error: ${err}`))
 
 module.exports = {
-    User: require('./User'),
-    Category: require('./Category'),
-    Book: require('./Book'),
+    User: require('./User.js'),
+    Category: require('./Category.js'),
+    Book: require('./Book.js'),
 }
